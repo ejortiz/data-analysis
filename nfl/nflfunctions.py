66ee4b9c5c5e48daa4ea82615e6e460e3607b9cc
ejortiz17 <eliud.j.ortiz@gmail.com>
2018-05-07 20:44:15 -0700

    first iteration of sacks to rushing yards

--- nfl/nflfunctions.py
+++ nfl/nflfunctions.py
@@ -8,4 +8,8 @@ def get_all_players_by_position(dataframe, player_position):
     result = dataframe[players_with_position]
     return result
 
-# def sum_player_stat_per_season(games_df, statistic_string)
+def sum_player_stats_per_season(games_df, stats_list):
+    return games_df.groupby(by=['player_id', 'year', 'team'])[stats_list].sum()
+
+def sum_stats_per_grouping(games_df, stats_to_group_by, stats_to_sum):
+    return games_df.groupby(by=stats_to_group_by)[stats_to_sum].sum()
--- nfl/sacks-to-yardage.py
+++ nfl/sacks-to-yardage.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 from nflfunctions import get_all_players_by_position
+from nflfunctions import sum_player_stats_per_season
+from nflfunctions import sum_stats_per_grouping
 
 import os
 file_path = os.path.dirname(os.path.abspath(__file__))
@@ -15,5 +19,29 @@ df_profiles = pd.read_csv(player_stats_full_path)
 rb_profiles = get_all_players_by_position(df_profiles, 'RB')
 
 df_games = pd.read_csv(games_full_path)
+
+print(df_games.info())
 rb_data = df_games[df_games["player_id"].isin(rb_profiles["player_id"])]
 
+rb_rushing_yards_df = sum_player_stats_per_season(rb_data, ['rushing_yards'])
+team_defensive_sacks = sum_stats_per_grouping(df_games, ['year', 'opponent'], ['defense_sacks'])
+
+# result_df = pd.merge(rb_rushing_yards_df, team_defensive_sacks, left_on='team', right_on='opponent')
+indexed_rb_df = rb_rushing_yards_df.stack().reset_index()
+indexed_sacks_df = team_defensive_sacks.stack().reset_index()
+
+# print(indexed_rb_df.info())
+# print(indexed_sacks_df.info())
+
+result_df = pd.merge(indexed_rb_df, indexed_sacks_df, left_on=['team', 'year'], right_on=['opponent', 'year'])
+# print(result_df.info())
+print(result_df.head())
+
+
+_ = plt.plot(result_df['0_y'], result_df['0_x'], linestyle='none', marker = '.')
+_ = plt.margins(0.02)
+_ = plt.xlabel("sacks per season")
+_ = plt.ylabel("RB Rushing Yards Per Season")
+plt.show()
+
+# print(result_df)

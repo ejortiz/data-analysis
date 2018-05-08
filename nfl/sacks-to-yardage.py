e1aea55faa84cb10604d7e2d31b617acb01f0342
ejortiz17 <eliud.j.ortiz@gmail.com>
2018-05-08 07:31:07 -0700

    filtering out of data before 1981 to get rid of pre-sack data

--- nfl/sacks-to-yardage.py
+++ nfl/sacks-to-yardage.py
@@ -20,28 +20,24 @@ rb_profiles = get_all_players_by_position(df_profiles, 'RB')
 
 df_games = pd.read_csv(games_full_path)
 
-print(df_games.info())
 rb_data = df_games[df_games["player_id"].isin(rb_profiles["player_id"])]
 
 rb_rushing_yards_df = sum_player_stats_per_season(rb_data, ['rushing_yards'])
 team_defensive_sacks = sum_stats_per_grouping(df_games, ['year', 'opponent'], ['defense_sacks'])
 
-# result_df = pd.merge(rb_rushing_yards_df, team_defensive_sacks, left_on='team', right_on='opponent')
-indexed_rb_df = rb_rushing_yards_df.stack().reset_index()
-indexed_sacks_df = team_defensive_sacks.stack().reset_index()
+indexed_rb_df = rb_rushing_yards_df.reset_index()
+indexed_sacks_df = team_defensive_sacks.reset_index()
 
-# print(indexed_rb_df.info())
-# print(indexed_sacks_df.info())
-
+indexed_rb_df = indexed_rb_df[indexed_rb_df["year"] > 1981]  # Sacks as an NFL stat only came into play after 1981
 result_df = pd.merge(indexed_rb_df, indexed_sacks_df, left_on=['team', 'year'], right_on=['opponent', 'year'])
-# print(result_df.info())
-print(result_df.head())
 
+sacks = result_df['defense_sacks']
+rushing_yards = result_df["rushing_yards"]
 
-_ = plt.plot(result_df['0_y'], result_df['0_x'], linestyle='none', marker = '.')
+_ = plt.plot(result_df['defense_sacks'], result_df['rushing_yards'], linestyle='none', marker = '.')
 _ = plt.margins(0.02)
-_ = plt.xlabel("sacks per season")
+_ = plt.xlabel("sacks allowed per season")
 _ = plt.ylabel("RB Rushing Yards Per Season")
 plt.show()
 
-# print(result_df)
+print(sacks.corr(rushing_yards))

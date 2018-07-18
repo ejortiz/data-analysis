59182ae9e558ae654ad3e2a60f762c7a5289275b
ejortiz <eliud.j.ortiz@gmail.com>
2018-07-18 07:58:17 -0700

    added column

--- nodeinfections/infection_summary.py
+++ nodeinfections/infection_summary.py
@@ -4,4 +4,8 @@ import matplotlib.pyplot as plt
 
 infection_df = pd.read_csv('Newman-SIR50-1mil-GS10-AD5-C04.csv')
 infection_df['InfectionProbability'] = infection_df['InfRate'] / 10000  # add extra column with InfectionProbability
+grouped_infection_df = infection_df.groupby()
+ # group the records by infection probability
 print(infection_df.tail())
+
+

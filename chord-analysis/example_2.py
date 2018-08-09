366cb12503928b95c4a0fe95ff8acb566ff51c05
ejortiz <eliud.j.ortiz@gmail.com>
2018-08-08 20:22:15 -0700

    categorizing chord data

--- chord-analysis/example_2.py
+++ chord-analysis/example_2.py
@@ -7,11 +7,11 @@ def get_chord_from_timestamp_df(row, chord_timestamp_df):
 
     chords = chord_timestamp_df.loc[chord_timestamp_df.iloc[:, 0] <= row['start']]
     chords = chords.loc[chords.iloc[:, 1] >= row['start'] + row['duration']]
+    if chords['chord'].size > 0:
+        return chords['chord'].iloc[0]
     # chord_val = chord_timestamp_df.iloc[:, 2].where(row['start'] >= chord_timestamp_df.iloc[:, 0] &
     #                                                 row['start'] +
     #                                                 row['duration'] <= chord_timestamp_df[:, 1])
-    print(chords.iloc[:, 2])
-    return chords.iloc[:, 2]
 
 # David bowie Space Oddity
 dfs = echo_nest_functions.get_dfs_from_json("echonest_chords/0086/echonest.json")
@@ -26,10 +26,11 @@ segments_df = dfs['segments']
 
 segments_df['chord'] = ''
 
-for index, row in segments_df.iterrows():
-    chords = chord_timestamp_df.loc[chord_timestamp_df['start_chord'] <= row['start']]
-    chords = chords.loc[chords['end_chord'] >= row['start'] + row['duration']]
-    if chords['chord'].size > 0:
-        row['chord'] = chords['chord'].iloc[0]
-        segments_df.iloc[index, :] = row
+# for index, row in segments_df.iterrows():
+#     chords = chord_timestamp_df.loc[chord_timestamp_df['start_chord'] <= row['start']]
+#     chords = chords.loc[chords['end_chord'] >= row['start'] + row['duration']]
+#     if chords['chord'].size > 0:
+#         row['chord'] = chords['chord'].iloc[0]
+#         segments_df.iloc[index, :] = row
+segments_df['chord'] = segments_df.apply(get_chord_from_timestamp_df, axis=1, args=(chord_timestamp_df,))
 segments_df.to_csv('test.csv')
